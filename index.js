7248926f3e7f7dc5693b0c4fedd67de9a5bf68e1
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-08-11 15:12:55 +0200

    Move classes to lib folder

---
+++ index.js
@@ -0,0 +1,4 @@
+const Employee = require("./tests/Employee.test");
+const Engineer = require("./tests/Engineer.test");
+const Intern = require("./tests/Intern.test");
+const Manager = require("./tests/Manager.test");
