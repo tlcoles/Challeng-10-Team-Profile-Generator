b2503c67f5cda5fbb7900aa07ec1e340aed99efa
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-08-12 18:01:08 +0200

    Add inquirer, express, and jest to project

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// Use built-in read & write modules
-const { readFileSync, writeFileSync } = require('fs')
+// Call inquirer CLI to prompt for information
+const inquirer = require('inquirer');
+const express = require('express');
 
 // Call classes employee, engineer, intern, and manager
 const Employee = require("./lib/employee");
@@ -8,8 +9,38 @@ const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
 
 
-const Binja = new Engineer ("Binja", 2, "binja@fauxco.de")
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        type: 'list',
+        message: 'Select employee type: ',
+        name: 'employee-type',
+        choices: ['manager', 'engineer', 'intern']
+    }
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+// Use built-in read & write modules
+const { readFileSync, writeFileSync } = require('fs')
+
+
+// Test with dummy people
+
+const Binja = new Engineer ("Binja", 2, "binja@fauxco.de", "binjacodes")
 const Tamara = new Manager ("Tamara", 1, "tamara@fauxco.de", 1)
+const Constantine = new Engineer ("Constantine", 3, "constantine@fauxco.de", "constantcodes")
+const Amelie = new Engineer ("Amelie", 4, "amelie@fauxco.de", "ameliecodes")
 
 // Create the index file and append the results with flag:a 
-writeFileSync("./dist/index.html",`Here is the result: ${Binja}, ${Tamara}`, {flag: "a"});
+writeFileSync("./dist/index.html",`Here is the result: ${Binja.name}, ${Tamara.id}`, {flag: "a"});
