3684aa0407fcaee06edb6bfbbc0eb0517540e7f0
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-08-12 14:16:24 +0200

    Deconstruct fs.readFileSync and fs.writeFileSync; call classes

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const Employee = require("./tests/Employee.test");
-const Engineer = require("./tests/Engineer.test");
-const Intern = require("./tests/Intern.test");
-const Manager = require("./tests/Manager.test");
+// Use built-in read & write modules
+const { readFileSync, writeFileSync } = require('fs')
+
+// Call classes employee, engineer, intern, and manager
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+
+
+const Binja = new Engineer ("Binja", 2, "binja@fauxco.de")
+const Tamara = new Manager ("Tamara", 1, "tamara@fauxco.de", 1)
+
+// Create the index file and append the results with flag:a 
+writeFileSync("./dist/index.html",`Here is the result: ${Binja}, ${Tamara}`, {flag: "a"})
