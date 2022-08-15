a705d60ad124e280e71cd20bc8225a95915ac955
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-08-15 16:02:35 +0200

    Collect employee data

--- index.js
+++ index.js
@@ -11,126 +11,115 @@ const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
+const { info } = require('console');
 
 // Start asking questions
 async function askQs() {
-// Give first employee a number and use createEmployee to return value
-var id = 0;
-var officeNumber = 0;
+    // Give first employee a number and use createEmployee to return value
+    var id = 0;
+    var officeNumber = 0;
 
-// Create array of basic questions
-const basicQs = [
-    {
-    type: 'input',
-    name: 'fullname',
-    message: 'What is their full name?',
-    when: newEmployee,
-    },
-    {
-    type: 'input',
-    name: 'email',
-    message: 'What is their email address?',
-    when: newEmployee,
-    },
-    {
-    type: 'list',
-    name: 'role',
-    message: 'Use the up/down arrow to select their new position: ',
-    choices: ['Intern', 'Engineer', 'Manager'],
-    default: 'Intern',
-    when: whichRole,
-    when: newEmployee,
-    },
-]
-
-const managerQ = 0    //! assign OfficeNumber as a numeric value
-
-var newEmployee = true;
-var employeeData = [];
-
-function whichRole(role) {
-  switch (role) {
-    case 'Manager':
-        var officeNumber = officeNumber++
-        break;
-    case 'Engineer':
-        inquirer.prompt (
-            {
-            type: 'input',
-            name: 'github',
-            message: 'What is their GitHub username?',
-            }
-        )
-        break;
-    case 'Intern':
-        inquirer.prompt (
-            {
-            type: 'input',
-            name: 'school',
-            message: 'What university are they attending?',
-            default: 'ESMT Berlin',
-            }
-        )
-        break;
-    default:
-        console.info("Your selection is invalid")
-        break;
-}
-}
-
-
-while (newEmployee) {
-    var id = id++
-    const yesOrNo = await inquirer 
-    .prompt (
+    // Create array of basic questions
+    const basicQs = [
         {
-        type: 'confirm',
-        name: 'createEmployee',
-        message: 'Create new employee file?',
-        default: true,
-        }
-    );
-    if (yesOrNo.createEmployee === false) {
-        break;
-    }
-
-    const newEmployee = await inquirer
-    .prompt (basicQs)
-    if (newEmployee.role === "Manager") {
-        let officeNumber = officeNumber++
-    } else if (newEmployee.role === "Engineer") {
-        inquirer.prompt (
-            {
-            type: 'input',
-            name: 'github',
-            message: 'What is their GitHub username?',
-            when: newEmployee,
-            }
-        )
-    } else { // defaults to Intern
-        inquirer.prompt (
-            {
-            type: 'input',
-            name: 'school',
-            message: 'What university are they attending?',
-            default: 'ESMT Berlin',
-            when: newEmployee,
-            }
-        )
-    }
-
-    employeeData.push (
+        type: 'input',
+        name: 'name',
+        message: 'What is their full name?',
+        when: enteringData,
+        },
         {
-        name: newEmployee.name,
-        id: '${id}',
-        email: newEmployee.email,
-        role: newEmployee.role,
-        office: '${officeNumber}',
-        github: newEmployee.github,
-        school: newEmployee.school,
+        type: 'input',
+        name: 'id',
+        message: 'What is their employee ID number?',
+        when: enteringData,
+        },
+        {
+        type: 'input',
+        name: 'email',
+        message: 'What is their email address?',
+        when: enteringData,
+        },
+        {
+        type: 'list',
+        name: 'role',
+        message: 'Use the up/down arrow to select their new position: ',
+        choices: ['Intern', 'Engineer', 'Manager'],
+        default: 'Intern',
+        when: enteringData,
+        },
+    ]
+
+    const managerQ = 0    //! assign OfficeNumber as a numeric value
+
+    var enteringData = true;
+    var employeeData = [];
+
+    while (enteringData) {
+        var id = id++
+        const yesOrNo = await inquirer 
+        .prompt (
+            {
+            type: 'confirm',
+            name: 'createEmployee',
+            message: 'Create new employee file?',
+            default: true,
+            }
+        );
+        if (yesOrNo.createEmployee === false) {
+            break;
         }
-    )
-}
+
+        const newEmployee = await inquirer
+        .prompt (basicQs)
+
+        if (newEmployee.role === "Manager") {
+            const officeAnswer = await inquirer.prompt (
+                {
+                type: 'input',
+                name: 'office',
+                message: 'What is their office number?',
+                // when: newEmployee,
+                }
+            )
+            newEmployee.office = officeAnswer.office // without this line, the answer is not attached to newEmployee
+        } else if (newEmployee.role === "Engineer") {
+           const githubAnswer = await inquirer.prompt (
+                {
+                type: 'input',
+                name: 'github',
+                message: 'What is their GitHub username?',
+                // when: newEmployee,
+                }
+            )
+            newEmployee.github = githubAnswer.github
+        } else { // defaults to Intern
+            const schoolAnswer = await inquirer.prompt (
+                {
+                type: 'input',
+                name: 'school',
+                message: 'What university are they attending?',
+                default: 'ESMT Berlin',
+                // when: newEmployee,
+                }
+            )
+            newEmployee.school = schoolAnswer.school
+        }
+
+        employeeData.push (
+            {
+            name: newEmployee.name,
+            id: newEmployee.id,
+            email: newEmployee.email,
+            role: newEmployee.role,
+            office: newEmployee.office,
+            github: newEmployee.github,
+            school: newEmployee.school,
+            }
+        )
+        console.info ("We expect this to display after the prompts")
+        console.info (newEmployee)
+        console.info (employeeData)
+    }
 }
 
 askQs();
